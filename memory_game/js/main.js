7e35f4ca780f1f6688eb37e4b0a0e0711eb63901
Intelligent Zombie <kristentaylor@Kristens-MacBook-Pro.local>
2018-11-18 18:30:22 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log ("User flipped" + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log ("User flipped" + cardTwo); 
-if (cardsInPlay[0] === cardsInPlay[2]) { 
-alert("You found a match!"); 
-} else {
-alert("Sorry,try again"); }
 
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay[0] === cardsInPlay[2]) { 
+		alert("You found a match!"); 
+	} else {
+		alert("Sorry,try again"); 
+		}
+}
+
+flipCard(0);
+flipCard(2);
+
+//User flipped cardOne;
+//User flipped cardTwo;
 
 
 
