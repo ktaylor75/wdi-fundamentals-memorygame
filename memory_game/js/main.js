2c927b63059c9de201074f792cfd7657d5950598
Intelligent Zombie <kristentaylor@Kristens-MacBook-Pro.local>
2018-11-24 16:36:11 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,54 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
-	if (cardsInPlay[0] === cardsInPlay[2]) { 
+var cardsInPlay = [];
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	this.setAttribute('src', cards[cardId].cardImage);
+ if (cardsInPlay[0] === cardsInPlay[2]) { 
 		alert("You found a match!"); 
 	} else {
 		alert("Sorry,try again"); 
-		}
-}
+		}	
+};
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute(i, 'data-id');
+		cardElement.addEventListener('click', flipCard);
+		cardElement.appendChild(game-board);
+	}
 
+};	
+
+createBoard();
+	
 //User flipped cardOne;
 //User flipped cardTwo;
 
